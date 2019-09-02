ec76c49ecdff1df0700d3891c2374244c1922707
George Mironov <yegor.mironovwill@gmail.com>
2019-09-03 01:10:44 +0300

    Changed code formating

--- Sandbox/src/main/java/us/tests/sandbox/newLO.java
+++ Sandbox/src/main/java/us/tests/sandbox/newLO.java
@@ -2,8 +2,8 @@ package us.tests.sandbox;
 
 public class newLO {
 
-	public static void main(String[] args){
-		System.out.println("Hi!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hi!");
+  }
 
 }
